25dba8277e3dbfdde8b9211f86572daf11caf83f
devendra <devendeveloper1@gmail.com>
2024-04-16 19:37:17 +0530

    final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,13 +56,10 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
-    implementation("com.github.kittinunf.fuel:fuel:2.3.1")
-    implementation("com.github.kittinunf.fuel:fuel-coroutines:2.3.1")
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.fuel)
     implementation(libs.gson)
 
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
